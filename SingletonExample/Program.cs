766adf7fba6a1011017b7b6ba94fbdae035e7105
Polina <tristiamika778@gmail.com>
2020-10-04 00:54:20 +0700

    Добавьте файлы проекта.

---
+++ SingletonExample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SingletonExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Запускаем паралельно, чтобы убедиться,
+            //что конструктор вызывается только один раз (один экземпляр)
+            Parallel.Invoke(
+               () => Check(),
+               () => Check()
+               );
+        }
+
+        private static void Check()
+        {
+            Singleton singleton = Singleton.Instance;
+        }
+    }
+}
---
+++ SingletonExample/Singleton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SingletonExample
+{
+    //Потокобезопасный вариант
+    //Полностью ленивая загрузка
+    //Быстрее некоторых других вариантов
+    //(нет дополнительных проверок и блокировок)
+    public sealed class Singleton
+    {
+        private Singleton()
+        {
+            Console.WriteLine($"Constructor");
+        }
+        public static Singleton Instance
+        {
+            get
+            {
+                Console.WriteLine($"Instance");
+                return Nested.instance;
+            }
+        }
+
+        private class Nested
+        {
+            //Статический конструктор вызовется
+            //при первом обращении к статическому члену класса
+            //и выполнится только один раз
+            static Nested() { }
+
+            //Модификатор internal даёт доступ для верхнего класса
+            internal static readonly Singleton instance = new Singleton();
+        }
+    }
+}
